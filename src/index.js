39df5b2931d58dc742d88593086fdf6580aef253
Brenobn <brenobragabn@gmail.com>
2023-10-17 16:30:18 -0300

    Simplified hero level determination

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-let heroName = "Iron DIO"
-let heroXp = 7.000
-let levelFerro = "Ferro"
-let levelOuro = "Ouro"
+let heroName = "Iron DIO";
+let heroXp = 6000;
+let level;
 
-  if(heroXp <= 1.000) {
-    console.log(`O herói ${heroName} está no nível ${levelFerro}`);
-  }
+if (heroXp <= 1000) {
+  level = "Ferro";
+} else if (heroXp <= 2000) {
+  level = "Bronze";
+} else if (heroXp <= 6000) {
+  level = "Prata";
+} else if (heroXp <= 7000) {
+  level = "Ouro";
+} else if (heroXp <= 8000) {
+  level = "Platina";
+} else if (heroXp <= 9000) {
+  level = "Ascendente";
+} else if (heroXp <= 10000) {
+  level = "Imortal";
+} else {
+  level = "Radiante";
+}
 
-  if(heroXp >= 7.000) {
-    console.log(`O herói ${heroName} está no nível ${levelOuro}`);
-  }
-
-  
- 
+console.log(`O herói ${heroName} está no nível ${level}`);
